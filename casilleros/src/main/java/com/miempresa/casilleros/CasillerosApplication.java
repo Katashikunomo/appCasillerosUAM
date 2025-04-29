f715e300fb322cce9b4d5fcab32266fbe45eddfc
Daniel Arellano <daniel.arellano@cua.uam.mx>
2025-04-28 22:32:21 -0600

    Estructura inicial del proyecto

---
+++ casilleros/src/main/java/com/miempresa/casilleros/CasillerosApplication.java
@@ -0,0 +1,13 @@
+package com.miempresa.casilleros;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CasillerosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CasillerosApplication.class, args);
+	}
+
+}
---
+++ casilleros/src/test/java/com/miempresa/casilleros/CasillerosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.miempresa.casilleros;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CasillerosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
